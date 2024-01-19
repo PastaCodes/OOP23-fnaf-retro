cce73b09589126f80a440a1e71dfe704d8133466
Marco Buda <marco.buda3@studio.unibo.it>
2024-01-19 19:19:42 +0100

    Tradotti in italiano i commenti nella main class

--- src/main/java/it/unibo/fnafretro/Program.java
+++ src/main/java/it/unibo/fnafretro/Program.java
@@ -1,15 +1,15 @@
 package it.unibo.fnafretro;
 
 /**
- * Utility class containing the program's entry-point.
- * @author Marco Buda
+ * Classe di utility contenente l'entry-point del programma.
+ * @author  Marco Buda
  */
 public final class Program {
     private Program() { }
 
     /**
-     * Program's entry-point.
-     * @param args
+     * L'entry-point del programma.
+     * @param   args    non utilizzati
      */
     public static void main(final String[] args) {
 
