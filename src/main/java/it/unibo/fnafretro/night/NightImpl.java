32914b5e7dd7433cb3409cb37396c241829d4ac0
Luca Ponseggi <luca.ponseggi@studio.unibo.it>
2024-02-11 18:01:06 +0100

    Aggiunte classi Night e NightImpl per l'avanzamento della notte

---
+++ src/main/java/it/unibo/fnafretro/night/NightImpl.java
@@ -0,0 +1,33 @@
+package it.unibo.fnafretro.night;
+
+import it.unibo.fnafretro.game.Game;
+
+public class NightImpl implements Night {
+    private int hour; // va da 0 a 6, la parte in cui viene scritto il numero va nella grafica
+    private final Game game;
+
+    public NightImpl(final Game game) {
+        hour = 0;
+        this.game = game;
+        
+        // passo a schedulerepeating i tick per ogni ora di gioco e una
+        // lambda con l'azione che deve eseguire al passare di ogni ora
+        this.game.events().scheduleRepeating(Night.TICKS_PER_GAME_HOUR, this::advance);
+
+    }
+
+    @Override
+    public void advance() {
+        this.hour++;
+        
+        if(this.hour == 6) {
+            // game.end(true);
+        }
+    }
+
+    @Override
+    public int getHour() {
+        return hour;
+    }
+
+}
