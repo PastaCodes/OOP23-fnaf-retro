244144e10b63f5209b06039534baba1d5e44a63b
Davide Sancisi <davide.sancisi@studio.unibo.it>
2024-02-11 15:21:43 +0100

    Implementazione completa dell'energia + test class

--- src/main/java/it/unibo/fnafretro/power/PowerImpl.java
+++ src/main/java/it/unibo/fnafretro/power/PowerImpl.java
@@ -1,45 +1,50 @@
 package it.unibo.fnafretro.power;
 
+import it.unibo.fnafretro.game.EventThread;
+import it.unibo.fnafretro.game.Game;
+
 /**
  * Implementazione dell'energia. 
  * @author Davide Sancisi
  */
 
 
-public class PowerImpl implements Power{
+public class PowerImpl implements Power {
 
-    private int ticks = 1;
+    private int energyTicks = 1;
     private double energyLevel = 1.000;
     final double ENERGY_TICK_COST = 0.002;
 
     @Override
     public void applyEnergyCost() {
-
         double totalEnergyCost;
-        totalEnergyCost = this.ticks*this.ENERGY_TICK_COST;
+        totalEnergyCost = this.energyTicks*this.ENERGY_TICK_COST;
         this.energyLevel -= totalEnergyCost;
-
-    }
-
-    @Override
-    public void init() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'init'");
     }
 
     @Override
     public void addTick() {
-        this.ticks++;
+        this.energyTicks++;
     }
 
     @Override
     public void removeTick() {
-        this.ticks--;
+        this.energyTicks--;
     }
 
     @Override
     public double getEnergyLevel() {
         return this.energyLevel;
+    }
+
+    @Override
+    public void init(Game game) {
+        game.events().scheduleRepeating(EventThread.TICKS_PER_SECOND, this::applyEnergyCost);
+    }
+
+    @Override
+    public int getTicks() {
+        return this.energyTicks;
     }
     
 }
