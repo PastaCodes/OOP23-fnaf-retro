7acc27a61f5a6756620af7348eacfab041e48aaa
FilippoDelBianco <60796355+FilippoDelBianco@users.noreply.github.com>
2024-02-11 17:59:42 +0100

    migliorato lo stile del codice di GameMap.java

--- src/main/java/it/unibo/fnafretro/map/GameMap.java
+++ src/main/java/it/unibo/fnafretro/map/GameMap.java
@@ -2,8 +2,30 @@ package it.unibo.fnafretro.map;
 
 import java.util.List;
 
-interface GameMap {
-    public Room getRoom(String roomName);
-    public Cameras getCameras();
-    public List<Room> getAdjacencies(Room room);
+/**
+ * rappresenta la mappa all'interno del gioco che contiene le stanze,
+ * le telecamere e come le stanze sono collegate fra di esse.
+ * @author Filippo Del Bianco
+ */
+public interface GameMap {
+
+    /**
+     * restituisce se esiste la stanza richiesta attraverso il nome della stanza.
+     * @param roomName nome della stanzache si vuole ottenere
+     * @return la stanza con il nome passato come input se esiste
+     */
+    Room getRoom(String roomName);
+
+    /**
+     * @return le telecamere presenti nella mappa di gioco
+     */
+    Cameras getCameras();
+
+    /**
+     * 
+     * @param room è la stanza della quale si volgiono sapere le stanze adiacenti
+     * @return le stanze adiacenti a quella fornita in input
+     * 
+     */
+    List<Room> getAdjacencies(Room room);
 }
