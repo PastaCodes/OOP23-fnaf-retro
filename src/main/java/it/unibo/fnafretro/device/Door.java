7e030f6c9405ac47d3855b5b2723b42a4fb243cb
Luca Ponseggi <luca.ponseggi@studio.unibo.it>
2024-02-12 01:18:21 +0100

    aggiunta Javadoc per la classe Door

--- src/main/java/it/unibo/fnafretro/device/Door.java
+++ src/main/java/it/unibo/fnafretro/device/Door.java
@@ -6,10 +6,14 @@ import it.unibo.fnafretro.power.Power;
  * Classe per la gestione della porta.
  * @author Luca Ponseggi
  */
-
 public class Door extends DeviceBase {
 
-    public Door(Power power) {
+    /**
+     * Costruisce un'istanza di Door che gestisce l'apertura e la chiusura di
+     * una porta.
+     * @param power gestione dell'energia
+     */
+    public Door(final Power power) {
         super(power);
     }
 
