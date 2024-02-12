475d47d8777826b3227bf67fc5a2fdc7ad8fe361
Luca Ponseggi <luca.ponseggi@studio.unibo.it>
2024-02-12 21:56:06 +0100

    Aggiunti i test per le classi Door e Lights

---
+++ src/test/java/it/unibo/fnafretro/device/TestDoor.java
@@ -0,0 +1,45 @@
+package it.unibo.fnafretro.device;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.fnafretro.power.Power;
+
+/**
+ * @author  Luca Ponseggi
+ */
+class TestDoor {
+
+    private Door porta;
+
+    @BeforeEach
+    void init() {
+        this.porta = new Door(Power.create());
+    }
+
+    @Test
+    void test() {
+        /*
+         * Il gioco inizia con la porta aperta (su off)
+        */
+        assertFalse(this.porta.isSwitchedOn());
+
+        /*
+         * Chiudo la porta
+         * controllo che sia chiusa
+         */
+        this.porta.switchOn();
+        assertTrue(this.porta.isSwitchedOn());
+
+        /*
+         * Apro la porta
+         * controllo che sia aperta
+         */
+        this.porta.switchOff();
+        assertFalse(this.porta.isSwitchedOn());
+    }
+
+}
---
+++ src/test/java/it/unibo/fnafretro/device/TestLights.java
@@ -0,0 +1,54 @@
+package it.unibo.fnafretro.device;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.fnafretro.power.Power;
+
+/**
+ * @author  Luca Ponseggi
+ */
+class TestLights {
+
+    private Lights luci;
+
+    @BeforeEach
+    void init() {
+        this.luci = new Lights(Power.create());
+    }
+
+    @Test
+    void test() {
+        /*
+         * Il gioco parta con le luci spente
+         */
+        assertFalse(this.luci.isLeftLightOn());
+        assertFalse(this.luci.isRightLightOn());
+
+        /*
+         * Accendo la luce sinistra
+         * controllo che si sia accesa
+         */
+        this.luci.switchOnLeftLight();
+        assertTrue(this.luci.isLeftLightOn());
+
+        /*
+         * Accendo la luce a destra: quella a sinistra dovrà spegnersi
+         */
+        this.luci.switchOnRightLight();
+        assertTrue(this.luci.isRightLightOn());
+        assertFalse(this.luci.isLeftLightOn());
+
+        /*
+         * Spengo entrambe le luci
+         * controllo che si siano spente
+         */
+        this.luci.switchOffLight();
+        assertFalse(this.luci.isLeftLightOn());
+        assertFalse(this.luci.isRightLightOn());
+    }
+
+}
