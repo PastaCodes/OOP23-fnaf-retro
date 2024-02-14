0ffdeb5bd24e953c8f17146aebbc9c821d4f0573
FilippoDelBianco <60796355+FilippoDelBianco@users.noreply.github.com>
2024-02-14 22:31:29 +0100

    aggiunti dei test

--- src/test/java/it/unibo/fnafretro/map/TestGameMap.java
+++ src/test/java/it/unibo/fnafretro/map/TestGameMap.java
@@ -5,27 +5,29 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class TestGameMap {
+/**
+ * @author  Filippo Del Bianco
+ */
+class TestGameMap {
 
-    private GameMap map;
+    private static final String MAIN_ROOM = "YOU";
 
     @Test void test1() {
-
-        map = new GameMapImpl();
+        final GameMap map = GameMap.create();
         assertEquals("1A", map.getRoom("1A").getRoomName());
         assertEquals("1B", map.getRoom("1B").getRoomName());
         assertEquals("5", map.getRoom("5").getRoomName());
         assertEquals("7", map.getRoom("7").getRoomName());
         assertEquals("3", map.getRoom("3").getRoomName());
-        assertEquals("YOU", map.getRoom("YOU").getRoomName());
+        assertEquals(MAIN_ROOM, map.getRoom(MAIN_ROOM).getRoomName());
 
         assertEquals("both", map.getRoom("1B").getSide());
         assertEquals("left", map.getRoom("3").getSide());
         assertEquals("right", map.getRoom("4A").getSide());
-        assertEquals("none", map.getRoom("YOU").getSide());
+        assertEquals("none", map.getRoom(MAIN_ROOM).getSide());
 
         assertEquals(List.of(map.getRoom("1B")), map.getAdjacencies(map.getRoom("1C")));
         assertEquals(List.of(map.getRoom("2B"), map.getRoom("3"), map.getRoom("1B")), map.getAdjacencies(map.getRoom("2A")));
-        assertEquals(List.of(map.getRoom("2B"), map.getRoom("4B")), map.getAdjacencies(map.getRoom("YOU")));
+        assertEquals(List.of(map.getRoom("2B"), map.getRoom("4B")), map.getAdjacencies(map.getRoom(MAIN_ROOM)));
     }
 }
