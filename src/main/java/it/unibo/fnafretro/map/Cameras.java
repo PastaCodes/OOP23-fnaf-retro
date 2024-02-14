c7e3240839705b01df3c46bd18ffc5ec4b79ad30
FilippoDelBianco <60796355+FilippoDelBianco@users.noreply.github.com>
2024-02-14 22:29:32 +0100

    aggiunta funzione per creare telecamere e cambiati i nomi dei metodi

--- src/main/java/it/unibo/fnafretro/map/Cameras.java
+++ src/main/java/it/unibo/fnafretro/map/Cameras.java
@@ -2,7 +2,7 @@ package it.unibo.fnafretro.map;
 
 /**
  * rappresenta le telecamere all'interno del gioco.
- * grazie ad esse all' interno del gioco si potrà visualizzare una sola stanza per volta
+ * grazie ad esse all' interno del gioco si potrÃ  visualizzare una sola stanza per volta
  * e potranno essere attive/disattive oppure attive ma temporaneamente disabilitate
  * @author Filippo Del Bianco
  */
@@ -11,34 +11,41 @@ public interface Cameras {
     /**
      * @return lo stato delle telecamere(spente o accese)
      */
-    boolean getStatus();
+    boolean areActive();
 
     /**
      * imposta lo stato delle telecamere(spente o accese).
-     * @param status lo stato che passiamo in input
+     * @param active lo stato che passiamo in input
      */
-    void setStatus(boolean status);
+    void setActive(boolean active);
 
     /**
      * @return se le telecamere sono nello stato greyscreen ossia temporaneamente disabilitate
      */
-    boolean getGreyScreen();
+    boolean areDisturbed();
 
     /**
      * per disabilitare o riabilitare temporaneamente le telecamere.
-     * @param greyScreen lo stato greyscreen passato come input
+     * @param disturbed lo stato greyscreen passato come input
      */
-    void setGreyScreen(boolean greyScreen);
+    void setDisturbed(boolean disturbed);
 
     /**
      * @return la stanza che stanno visualizzando le telecamere
      * 
      */
-    Room getCurrentRoom();
+    String getCurrentRoom();
 
     /**
      * per impostare la stanza da far visualizare dalle telecamere.
      * @param newRoom la stanza da far visualizzare alle telecamere
      */
-    void setCurrentRoom(Room newRoom);
+    void setCurrentRoom(String newRoom);
+
+    /**
+     * @return  un'istanza delle telecamere nella mappa di gioco
+     */
+    static Cameras create() {
+        return new CamerasImpl("1A");
+    }
 }
